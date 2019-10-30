853e9435f19a156e636c284e819efdf47cc72d98
Doosik Kim <doosik@etri.re.kr>
2019-10-30 17:57:46 +0900

    Add more configuration.

--- runme.bat
+++ runme.bat
@@ -1,1 +1,6 @@
-msbuild myproject.vcxproj /p:configuration=debug
+msbuild myproject.vcxproj /t:clean
+
+msbuild myproject.vcxproj /p:configuration=debug   /p:platform=win32
+msbuild myproject.vcxproj /p:configuration=release /p:platform=win32
+msbuild myproject.vcxproj /p:configuration=debug   /p:platform=x64
+msbuild myproject.vcxproj /p:configuration=release /p:platform=x64
